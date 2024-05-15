fcba5ec66359e39f05306c8334f08ce9d1f5aeef
yulunez <yaru75665@gmail.com>
2024-05-15 16:47:42 -0500

    creacion de mcdonalds

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const navLinks = document.querySelector('.nav-links');
 toggleBtn.addEventListener('click',()=> {
     navLinks.classList.toggle('active');
 });
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const offsetTop = targetElement.offsetTop;
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
